2f91de063b4997197cac121f13303aa8e372fec3
Nina Bucic <bucicnina@gmail.com>
2024-12-30 19:39:00 +0100

    3-tier architecture setup

---
+++ Drive/Drive.Presentation/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Drive.Presentation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
